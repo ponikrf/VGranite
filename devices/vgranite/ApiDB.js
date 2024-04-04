39dbf2f4f4e79ab61f6dd136d86007ed8373018d
Boris Bobylev <ponik_rf@mail.ru>
2024-04-04 11:20:44 +0400

    Update ApiDB Apidoc + move schemes settings to service

--- devices/vgranite/ApiDB.js
+++ devices/vgranite/ApiDB.js
@@ -28,6 +28,11 @@ module.exports = class extends ApiDevice {
   checkOptions(){
     return {
         inputs: Rule.number().default(4).require().description('Number of metrics inputs'),
+        shcemes: Rule.array().default([]).content(Rule.object().fields({
+          shceme: Rule.string().require().description('Scheme name').example('test'),
+          pattern: Rule.string().require().description('Path pattern').example('test.*'),
+          retention: Rule.string().require().description('Retention settings').example('1m:24h,5m:7d,1h:1mon,6h:1y'),
+        })).require().description('Schemes settings {scheme, pattern, retention}')
     }
   }
 
@@ -47,11 +52,35 @@ module.exports = class extends ApiDevice {
   DB = new Database()
 
   process(){
-    this.DB.scheme('system', 'system.*', '5s:1h,1m:6h,5m:1d,30m:1w,1h:1mon,12h:1y')
-    this.DB.scheme('connect', 'connect.*', '1m:24h,5m:7d,1h:1mon,6h:1y')
-    this.DB.scheme('traffic', 'traffic.*', '1m:24h,5m:7d,1h:1mon,6h:1y')
+    for (const scheme of this.options.shcemes){
+      this.DB.scheme(scheme.scheme, scheme.pattern, scheme.retention)      
+    }
   }
 
+  /**
+   * @api {post} /database/read Read from database
+   * @apiName DatabaseRead
+   * @apiGroup Database
+   * 
+   * @apiParam {String} path Metric path
+   * @apiParam {String} period Period string ('now-1h:now')
+   * @apiParam {String | Number} precision ('5s', 123,'1m') 
+   * @apiParam {String} func Aggregate function ('last','min','max','first','avg')
+   * 
+   * @apiSuccess {Number} relevant
+   * @apiSuccess {Number} start
+   * @apiSuccess {Number} end 
+   * @apiSuccess {Array<IMetric>} rows
+   *  
+   * @apiSuccessExample Success-Response:
+   * HTTP/1.1 200 OK
+   * 
+   *  { 
+   *    "relevant": true, "start": 1712202660, "end": 1712213460,
+   *    "rows": [{ "time": 1712202660, "value": 0 },
+   *            { "time": 1712213460, "value": 0 }
+   *   ]}
+  */
   POSTRead(express){
     const { error, value } = this.readScheme.validate(express.req.body)
     this.validationResponse(error)
@@ -59,7 +88,14 @@ module.exports = class extends ApiDevice {
   }
 
   /**
-   * [time, value, path]
+   * Input for metric dynamic ports 
+   * @see inputs
+   * 
+   * format:
+   * [time (in second - integer), value (float, integer), path (string)],
+   * 
+   * @example
+   * [123123123, 1.234, "path.to.metric"]
   */
   inputMetric (data) {
     if (typeof data[1] === 'string') data[1] = parseFloat(data[1])
